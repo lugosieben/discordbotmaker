63197e70d7b121046871941b5f4b132b3588e0a4
lugosieben <62535192+lugosieben@users.noreply.github.com>
2020-12-05 10:44:18 +0100

    Adding command event

--- main.js
+++ main.js
@@ -7,26 +7,36 @@ const Emitter = require('emitjs');
 
 class Bot {
     constructor(config) {
-        if(!config.token) throw new TypeError('No token given for bot')
+        if(!config.token) throw new TypeError('No token given for bot') // Config error messages
         if(!config.prefix) throw new TypeError('No prefix given for bot')
 
-        this.token  = config.token;
+        this.token  = config.token; // Bot settings
         this.client = new Client();
         this.prefix = config.prefix;
         this.events = new Emitter()
         
         this.client.login(this.token)
         this.client.on('ready', () => {
-            console.log('Your bot ' + this.client.user.tag + ' is online now!')
+            console.log('Your bot ' + this.client.user.tag + ' is online now!') // Online Notification
         })
+
+        // Event Handling with `emitjs`
         this.client.on('message', async message => {
-            const author = new User(message.author)
-            const channel = new Channel(message.channel)
-            const msg = new Message(author, channel, message.toString())
-            this.events.emit('message', msg)
+            if(message.author.bot) return; // Checks if author is bot
+            const author = new User(message.author) // Message Author
+            const channel = new Channel(message.channel) // Message Channel
+            const msg = new Message(author, channel, message.toString()) // Message It-Self
+            this.events.emit('message', msg) // Emits message recieved event
+            let args = message.content.slice(config.prefix.length).split(' ');
+            let command = args.shift().toLowerCase();
+            if(msg.content.startsWith(this.prefix)) this.events.emit('command', msg, command, args)
             if(config.disableMessageNotis || config.disableMessageNotifications || config.dmn) this.notis = false
             else this.notis = true
-            if(this.notis) console.log('[Recieved Message] By ' + message.author.tag + ' with ID ' + message.author.id + ': ' + message.toString())
+            if(this.notis) console.log('[Recieved Message] By ' + message.author.tag + ' with ID ' + message.author.id + ': ' + message.toString()) // Message Notis
+        })
+        this.client.on('ready', () => {
+            this.events.emit('ready') // Emits ready event
+            this.events.emit('online') // Alias of ready event
         })
     }
 }
